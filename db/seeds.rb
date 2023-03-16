dca697e5701cef51b183552cd66df480c2f9e119
James Parrish <jthomasparrishjr@gmail.com>
2023-03-15 19:15:56 -0500

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create!(title: "Pulp Fiction", director: "Quentin Tarantino", genre: "Crime", year: 1994, image_url:"https://en.wikipedia.org/wiki/File:Pulp_Fiction_(1994)_poster.jpg" ) 
+Movie.create!(title: "Young Frankenstein", director: "Mel Brooks", genre: "Comedy", year: 1974, image_url:"https://en.wikipedia.org/wiki/File:Young_Frankenstein_movie_poster.jpg")
+Movie.create!(title: "Tombstone", director: "George P. Cosmatos", genre: "Western", year: 1993, image_url:"https://en.wikipedia.org/wiki/File:Tombstoneposter.jpeg")
+Movie.create!(title: "Good Will Hunting", director: "Gus Van Sant", genre: "Drama", year: 1997, image_url: "https://en.wikipedia.org/wiki/File:Good_Will_Hunting.png") 
+
+
+
