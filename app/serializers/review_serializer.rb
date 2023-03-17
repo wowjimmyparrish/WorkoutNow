d070aa5bbdc7d5fad9504ba2931cd8641b5748a0
James Parrish <jthomasparrishjr@gmail.com>
2023-03-16 19:48:44 -0500

    updated routes

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :comment, :rating, :movie_id, :user_id
+  belongs_to :movie 
+  belongs_to :user 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  
-  resources :users
   resources :reviews
-  resources :movies
+
+  resources :movies, only: [:show, :index, :create] do
+    resources :reviews, only: [:show, :index] 
+  end
+
+ post "/signup", to: "users#create"
+ get "/me", to: "users#show"
+ post "/login", to: "sessions#create"
+ delete "/logout", to: "sessions#destroy"
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
