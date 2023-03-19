8fcf1f47a99515d076beaadd580d8f34d14890e8
James Parrish <jthomasparrishjr@gmail.com>
2023-03-19 18:02:58 -0500

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,9 @@ Movie.create!(title: "Young Frankenstein", director: "Mel Brooks", genre: "Comed
 Movie.create!(title: "Tombstone", director: "George P. Cosmatos", genre: "Western", year: 1993, image_url:"https://en.wikipedia.org/wiki/File:Tombstoneposter.jpeg")
 Movie.create!(title: "Good Will Hunting", director: "Gus Van Sant", genre: "Drama", year: 1997, image_url: "https://en.wikipedia.org/wiki/File:Good_Will_Hunting.png") 
 
+User.create!(username: "jimmy", password: "password")
+User.create!(username: "darrel", password: "password123")
 
+Review.create!(comment: "Great movie!", rating: 5, user_id: 1, movie_id: 1) 
+Review.create!(comment: "Bad movie!", rating: 2, user_id: 2, movie_id: 2)
 
