f4db6998394798ccffa44bc90f92557f7e01d639
James Parrish <jthomasparrishjr@gmail.com>
2023-03-20 18:46:44 -0500

    updated users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: :create
+    skip_before_action :authorize, only: :create, :show
 
     def create 
         user = User.create!(user_params)
         session[:user_id] = user.id
         render json: user, status: :created
+    end
+
+    def show 
+        render json: @current_user
     end
 
     private 
