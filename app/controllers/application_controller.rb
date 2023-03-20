3335d33281ae0f9fe302c2437d027441295b1562
James Parrish <jthomasparrishjr@gmail.com>
2023-03-20 18:26:02 -0500

    sessions controller update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize 
+
+  private 
+
+  def authorize 
+    @current_user = User.find_by(id: session[user_id])
+    render json: {error: "Not authorized, please log in."}, unless @current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:show, :index] 
   end
 
+  resources :users 
  post "/signup", to: "users#create"
  get "/me", to: "users#show"
  post "/login", to: "sessions#create"
