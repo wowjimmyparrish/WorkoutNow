90aed61bbca9c9aeb83638ca747a1fa31d92df38
James Parrish <jthomasparrishjr@gmail.com>
2023-03-29 23:02:17 -0500

    fixed schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 User.create!(username: "jimmy", password: "password")
 User.create!(username: "darrel", password: "password123")
 
-Workout.create!(title: "Bicep Blaster", focus: "arms", length: 45, workout: "Concentration Curls 5 reps x 5 sets, Barbell Curls 5 reps x 5 sets, Hammer Curls 10 reps x 3 sets", user_id: 1)
-Workout.create!(title: "Redwoods", focus: "legs", length: 60, workout: "Squats 10 reps x 5 sets, Lunges 10 reps x 3 sets, GHR 10 reps x 3 sets, Leg extensions 10 reps x 3 sets", user_id: 2) 
-Workout.create!(title: "Tombstone", focus: "full body", length: 90, workout: "Squats 5 reps x 5 sets, Deadlifts 5 reps x 5 sets, Bench Press 5 reps x 5 sets, Barbell Rows 5 reps x 5 sets, Pullups AMRAP x 3, Military Press 5 reps x 5 sets", user_id: 1)
-Workout.create!(title: "Arm Farm", focus: "arms", length: 45, workout: "Barbell Curls 10 reps x 3 sets, Hammer Curls 10 reps x 3 sets, Skullcrushers 10 reps x 3 sets, Tricep extensions 10 reps x 3 sets", user_id: 2)
+Workout.create!(title: "Bicep Blaster", focus: "arms", length: 45, workout: "Concentration Curls 5 reps x 5 sets, Barbell Curls 5 reps x 5 sets, Hammer Curls 10 reps x 3 sets")
+Workout.create!(title: "Redwoods", focus: "legs", length: 60, workout: "Squats 10 reps x 5 sets, Lunges 10 reps x 3 sets, GHR 10 reps x 3 sets, Leg extensions 10 reps x 3 sets") 
+Workout.create!(title: "Tombstone", focus: "full body", length: 90, workout: "Squats 5 reps x 5 sets, Deadlifts 5 reps x 5 sets, Bench Press 5 reps x 5 sets, Barbell Rows 5 reps x 5 sets, Pullups AMRAP x 3, Military Press 5 reps x 5 sets")
+Workout.create!(title: "Arm Farm", focus: "arms", length: 45, workout: "Barbell Curls 10 reps x 3 sets, Hammer Curls 10 reps x 3 sets, Skullcrushers 10 reps x 3 sets, Tricep extensions 10 reps x 3 sets")
 
 Review.create!(comment: "Great workout!", rating: 5, user_id: 1, workout_id: 1) 
 Review.create!(comment: "Bad workout!", rating: 2, user_id: 2, workout_id: 2)
