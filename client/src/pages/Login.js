8fcbe4da99969bf7f567364284fafd740de1a2d7
James Parrish <jthomasparrishjr@gmail.com>
2023-07-20 06:49:06 -0500

    update

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import LoginForm from "../components/LoginForm";
 import SignUpForm from "../components/SignUpForm";
-import "../index.css";
 function Login({ setUserWorkouts, setUserReviews }) {
   const [showLogin, setShowLogin] = useState(true);
 
